e89fa3ef8e86fcfda480e5a85baa4814b0d8d770
linxue <linxue.sia@gmail.com>
2019-04-15 17:30:57 +0800

    change

--- about.md
+++ about.md
@@ -2,11 +2,23 @@
 
 ## 学爬虫前的基础
 ### requests 网络请求库
+
 ### 正则表达式
-### html 基础
-### python 的文件 I/O 操作
+python 内置的 re 模块，拥有一系列方法用于正则表达式匹配和替换，用于快速查找和匹配字符串。
+参考地址： [http://www.runoob.com/python/python-reg-expressions.html]
+
+### python open 方法操作文件
+python 内置的 open 方法可以打开一个文件，并创建一个 file 对象，对其进行读写操作。
+参考地址：[http://www.runoob.com/python/python-func-open.html]
+
 ### lxml 与 Xpath
-### 多线程爬虫
+XPath 是一门在 XML 文档中查找信息的语言。XPath 用于在 XML 文档中通过元素和属性进行导航，它使用路径表达式来选取 XML 文档上的节点，与系统中的文件路径类似。
+lxml 类库是一个 Html/XML 的解析器，并且解析效率比较高。
+文档链接：
+Xpath - [http://www.w3school.com.cn/xpath/xpath_intro.asp]
+lxml - [https://lxml.de/index.html]
+
+### 多线程概念
 
 ## 练习 demo
 从小说网站爬取聊斋志异全文的内容，并存储到一个 txt 文件中。
@@ -17,6 +29,10 @@
 * 获取每章节的链接地址
 * 访问每一章节，分析章节内容的 html 内容
 * 新建一个文本文件，将每章节的文本写入
+
+其他思考：
+#### 通过打时间戳查看每个请求的耗时
+#### 通过调整线程池的大小缩短爬虫时间
 
 
 
--- app.py
+++ app.py
@@ -1,38 +1,71 @@
 import requests as rq
 import re
-import lxml
+import lxml.html
 from bs4 import BeautifulSoup
 from multiprocessing.dummy import Pool
 import time
 
-def get_urls(base_url, page_url):
-    response = rq.get(page_url)
+
+def get_html(url):
+    """
+    通过链接地址获取页面的 html 源码
+    :param url: 页面链接
+    :return: 页面 html 源码 string 格式
+    """
+    response = rq.get(url)
     response.encoding = response.apparent_encoding
     html_str = response.text
+    return html_str
 
-    """
-    第一种方式 使用bs
-    """
-    soup = BeautifulSoup(html_str, 'html.parser')
-    content_body = soup.find(class_='content')
-    titles = content_body.find_all('a')
 
+def get_urls(base_url, page_url):
     """
-    第二种方式 使用Xpath
+    获取每个章节的 url 地址
+    :param base_url: 网站根路径
+    :param page_url: 页面链接
+    :return: 返回每个章节的链接组成的列表
     """
-
+    startt = time.time()
+    html = get_html(page_url)
     url_lst = []
-    for title in titles:
-        url_lst.append(base_url+title['href'])
+
+    # 第一种方式 使用bs
+    # bss = time.time()
+    # soup = BeautifulSoup(html, 'html.parser')
+    # content_body = soup.find(class_='content')
+    # titles = content_body.find_all('a')
+    # for title in titles:
+    #     url_lst.append(base_url+title['href'])
+    # bse = time.time()
+    # print(f'使用bs方法获取总共耗时{bse - bss}')
+
+    # 第二种方式 使用Xpath 效率大概快了0.005秒
+    xps = time.time()
+    selector = lxml.html.fromstring(html)
+    urls = selector.xpath('//td[@class="content"]/table/tr/td/a/@href')
+    for title in urls:
+        url_lst.append(base_url+title)
+    xpe = time.time()
+    print(f'使用xpath方法获取总共耗时{xpe - xps}')
+
+    endt = time.time()
+    print(f'获取页面所有的链接总共耗时{endt - startt}')
     return url_lst
 
 
 def get_content(url):
-    response = rq.get(url)
-    response.encoding = response.apparent_encoding
-    html_str = response.text
-    content = re.search('class=content colSpan=3>(.*?)</td>', html_str, re.S).group(1)
+    """
+    通过链接获取每个章节的文本内容
+    :param url: 页面的链接
+    :return: 章节的文本 string 格式
+    """
+    # startt = time.time()
+    html = get_html(url)
+
+    content = re.search('class=content colSpan=3>(.*?)</td>', html, re.S).group(1)
     content = content.replace('<BR>', '\n').strip()
+    # endt = time.time()
+    # print(f'获取一个章节的内容耗时{endt - startt}')
     return content
 
 
@@ -43,7 +76,7 @@ if __name__ == '__main__':
         pool = Pool(10)
         chapters = pool.map(get_content, chapter_lst)
         end = time.time()
-        print(f'爬取总耗时{end - start}')
+        print(f'爬取总耗时{end - start}， 总共章节数{len(chapter_lst)}')
         for item in chapters:
             f.write(item)
     exit()
